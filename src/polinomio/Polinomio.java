a866f04751bbe248325d43d6c6bdd24137b12961
Manuel <manuelharo1994@gmail.com>
2016-11-11 20:20:03 -0500

    Interfaz hecha

--- src/polinomio/Polinomio.java
+++ src/polinomio/Polinomio.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package polinomio;
 
-/**
- *
- * @author manuel
- */
 public class Polinomio {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        
     }
-    
 }
